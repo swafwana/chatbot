c5e452299b08baf634f339a707a0e711a3f39a87
SWAFWANA SHERI <swafwanasheri17@gmail.com>
2026-06-10 14:58:58 +0530

    db Changed to sqlite for deployement

--- database.py
+++ database.py
@@ -1,21 +1,37 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import declarative_base, sessionmaker
+
+# load_dotenv()
+
+#  DATABASE_URL = os.getenv("DATABASE_URL")
+
+# if not DATABASE_URL:
+#     raise ValueError("DATABASE_URL is not set in .env")
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# Base = declarative_base()
+
+
+
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
+DATABASE_URL = "sqlite:///./chatbot.db"
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in .env")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
-
-
-
 
 def get_db():
     db = SessionLocal()
--- reset_db.py
+++
@@ -1,6 +0,0 @@
-from database import engine, Base
-import models
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
-print("DB recreated.")
